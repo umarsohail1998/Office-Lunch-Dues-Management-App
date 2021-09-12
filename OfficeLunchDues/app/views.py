e94e697537a432f8858c41a5fe6f157d3b094bf4
M. Umar Sohail <umarsohail1998@gmail.com>
2021-09-12 19:32:10 +0500

    New file added

--- OfficeLunchDues/app/views.py
+++ OfficeLunchDues/app/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('hello worl welcome to app')
+
+def next(request):
+    return HttpResponse('hello worl welcome to app')
